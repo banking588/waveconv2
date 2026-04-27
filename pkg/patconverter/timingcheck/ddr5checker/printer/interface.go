bc2df1efb9e30c6d9a3254686fc445e85695dce1
banking588 <banking588@hotmail.com>
2026-04-27 10:39:21 +0800

    Add Printer interface with conversion methods

--- pkg/patconverter/timingcheck/ddr5checker/printer/interface.go
+++ pkg/patconverter/timingcheck/ddr5checker/printer/interface.go
@@ -1,1 +1,16 @@
 package printer
+
+import (
+	"waveconv/pkg/patconverter/timingcheck/ddr5checker/core"
+	"waveconv/pkg/patconverter/timingcheck/ddr5checker/model"
+	"waveconv/pkg/patconverter/timingcheck/ddr5checker/model/mrstatusreg"
+)
+
+type Printer interface {
+	model.Formatter
+	Convert(nodes []*core.SeqNode) []*model.PrintNode
+	ConvertWithCheck(nodes []*core.SeqNode, checkResult *model.CheckResult) []*model.PrintNode
+	SetWayCount(n int)
+	SetLoopStrategy(s LoopStrategy)
+	SetMRS(mrs mrstatusreg.MRS)
+}
