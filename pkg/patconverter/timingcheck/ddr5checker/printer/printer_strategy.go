0cfa00f9b67b411a65ec48cd9d1231799d7cec49
banking588 <banking588@hotmail.com>
2026-04-27 12:05:50 +0800

    Add PrinterStrategy interface definition

--- pkg/patconverter/timingcheck/ddr5checker/printer/printer_strategy.go
+++ pkg/patconverter/timingcheck/ddr5checker/printer/printer_strategy.go
@@ -1,1 +1,13 @@
 package printer
+
+import (
+	"waveconv/pkg/patconverter/timingcheck/ddr5checker/core"
+)
+
+// PrinterStrategy 定義 wayCount ≤ 1 和 wayCount > 1 的不同處理流程
+type PrinterStrategy interface {
+	ProcessCommand(nodes []*core.SeqNode, bp *basePrinter, acc *accumulator, i int, cmdIndex int) (int, int)
+	ProcessLoop(nodes []*core.SeqNode, bp *basePrinter, acc *accumulator, i int, mergedLabel string) int
+	ShouldPad() bool
+	SetLoopStrategy(ls LoopStrategy)
+}
