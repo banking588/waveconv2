0da0f9f98f8a9293ba8350078e68721dfef5d099
banking588 <banking588@hotmail.com>
2026-04-27 10:42:57 +0800

    Add LoopStrategy interface and LoopContext struct
    Define LoopStrategy interface and LoopContext struct for loop processing.

--- pkg/patconverter/timingcheck/ddr5checker/printer/loop_strategy.go
+++ pkg/patconverter/timingcheck/ddr5checker/printer/loop_strategy.go
@@ -1,1 +1,31 @@
 package printer
+
+import (
+	"waveconv/pkg/patconverter/timingcheck/ddr5checker/core"
+	"waveconv/pkg/patconverter/timingcheck/ddr5checker/model"
+)
+
+// LoopStrategy 定義 Loop/JNI/IDXI 的封裝策略
+type LoopStrategy interface {
+	ProcessLoop(ctx *LoopContext) int
+	ResolveJNINode(ctx *LoopContext, label string)
+	FlushAsIDXI(ctx *LoopContext, srs []model.SubRow, repeatCount int)
+	IsLoopOperator(op string) bool
+}
+
+// LoopContext 把 Loop 處理需要的上下文集中在一起
+type LoopContext struct {
+	BP          *basePrinter
+	Acc         *accumulator
+	Node        *core.SeqNode
+	MergedLabel string
+}
+
+func NewLoopContext(bp *basePrinter, acc *accumulator, node *core.SeqNode, mergedLabel string) *LoopContext {
+	return &LoopContext{
+		BP:          bp,
+		Acc:         acc,
+		Node:        node,
+		MergedLabel: mergedLabel,
+	}
+}
