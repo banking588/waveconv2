3f33ce79adbce4910136e828eb0ba8614cbe17df
banking588 <banking588@hotmail.com>
2026-04-27 10:38:18 +0800

    Add commandToSubRows and expandSubRowGroup functions

--- pkg/patconverter/timingcheck/ddr5checker/printer/command.go
+++ pkg/patconverter/timingcheck/ddr5checker/printer/command.go
@@ -1,1 +1,36 @@
 package printer
+
+import (
+	"waveconv/pkg/patconverter/timingcheck/ddr5checker/core"
+	"waveconv/pkg/patconverter/timingcheck/ddr5checker/model"
+)
+
+func (bp *basePrinter) commandToSubRows(node *core.SeqNode) []model.SubRow {
+	srs := bp.config.RowMapCmd(node.Cmd, bp.config.WayCount)
+	for i := range srs {
+		srs[i].SourceLineNum = node.LineNum
+	}
+	return srs
+}
+
+func expandSubRowGroup(sr model.SubRow, status model.SubRowStatus) []model.SubRow {
+	macros := sr.EffectiveMacros()
+	expanded := make([]model.SubRow, len(macros))
+	for i, m := range macros {
+		expanded[i] = model.SubRow{
+			Macro:         m,
+			Addr:          sr.Addr,
+			Settings:      sr.Settings,
+			SF:            sr.SF,
+			XYC:           sr.XYC,
+			Timing:        sr.Timing,
+			TimingGroup:   sr.TimingGroup,
+			Cmd:           sr.Cmd,
+			Status:        status,
+			Violation:     sr.Violation,
+			RepeatCnt:     0,
+			SourceLineNum: sr.SourceLineNum,
+		}
+	}
+	return expanded
+}
